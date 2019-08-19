0a0d0f4019c7d45458bdd089743af7ac440c5800
unknown <tcutlip08@gmail.com>
2019-08-19 16:37:45 -0400

    env file works, connected a few other things needed to be required

--- run.js
+++ run.js
@@ -1,1 +1,33 @@
+require("dotenv").config();
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
 var password = process.env.password
+
+console.log(password);
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: password,
+    database: 'Bamazon_DB'
+});
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+});
+connection.query('SELECT * FROM products', function (err, res) {
+    if (err) throw err;
+    console.log(res);
+});
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+]).then(function (data) {
+    console.log(data);
+});
